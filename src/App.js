adcc11af9df500a74367aef2f21cc9c5500def29
@farissahman1 <farissahman1@gmail.com>
2021-11-26 18:49:48 +0100

    created seperated slices

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Counter from './components/Counter';
-
+import { useSelector } from "react-redux";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import Counter from "./components/Counter";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <>
+      <Header />
+      {auth ? <UserProfile /> : <Auth />}
+      <Counter />
+    </>
   );
 }
 
